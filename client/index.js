44661d0edbc26d6b6abe919f970fdf66270d79d3
Your Name <you@example.com>
2024-01-28 21:09:47 +0100

    initial commit

---
+++ client/index.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+const prompt = require("prompt-sync")();
+
+const serverUrl = 'http://localhost:1225';
+
+async function main() {
+  // TODO: how do we prove to the server we're on the nice list? 
+  const name = prompt("Enter a name for a check:");
+  if (name == null) {
+    console.log("You have to enter a name");
+  }
+  const tree = new MerkleTree(niceList);
+  const index = niceList.findIndex(n => n === name);
+  const proof = tree.getProof(index);
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    // TODO: add request body parameters here!
+    name,
+    proof
+  });
+
+  console.log( {name, gift} );
+}
+
+main();
